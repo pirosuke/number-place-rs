6075fe49bdb3dc274af97d78908cfff8ecfa56ef
htanaka <pirosukke@gmail.com>
2021-03-14 22:20:37 +0900

    fixed problem generator

--- number-place-problem-generator/src/main.rs
+++ number-place-problem-generator/src/main.rs
@@ -1,11 +1,14 @@
 extern crate number_place_lib;
 
-use std::fs::File;
-use std::io::BufReader;
+use rand::seq::SliceRandom;
+use std::fs::{read_dir, File};
+use std::path::{Path, PathBuf};
+use std::io::{BufReader, BufWriter, Write};
+use std::ffi::OsStr;
 
 use clap::{App, Arg};
 
-fn fit_template(pattern_lines: Vec<Vec<i32>>, template_lines: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+fn fit_template(pattern_lines: &Vec<Vec<i32>>, template_lines: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut fit_lines: Vec<Vec<i32>> = Vec::new();
     for row_zip in pattern_lines.iter().zip(template_lines.iter()) {
         let (pattern_row, template_row) = row_zip;
@@ -42,38 +45,76 @@ fn is_solvable(problem_lines: &Vec<Vec<i32>>) -> bool {
 fn main() {
     let cli_options = App::new("number_place_problem_generator")
         .about("Number Place Problem Generator")
-        .arg(Arg::with_name("pattern")
-            .help("Pattern JSON File Path")
-            .long("pattern")
+        .arg(Arg::with_name("pattern_dir")
+            .help("Pattern JSON Dir Path")
+            .long("pattern_dir")
             .short("p")
             .required(true)
             .takes_value(true)
         )
-        .arg(Arg::with_name("template")
-            .help("Template JSON File Path")
+        .arg(Arg::with_name("template_dir")
+            .help("Template JSON Dir Path")
             .long("template")
             .short("t")
             .required(true)
             .takes_value(true)
         )
+        .arg(Arg::with_name("output_dir")
+            .help("Output Dir Path")
+            .long("output")
+            .short("o")
+            .required(true)
+            .takes_value(true)
+        )
         .get_matches();
     
-    let p_pattern_file_path = cli_options.value_of("pattern").unwrap();
-    let pattern_file = File::open(p_pattern_file_path).unwrap();
-    let pattern_reader = BufReader::new(pattern_file);
-    let pattern_lines: Vec<Vec<i32>> = serde_json::from_reader(pattern_reader).unwrap();
+    let p_pattern_dir_path = cli_options.value_of("pattern_dir").unwrap();
+    let pattern_path_list: Vec<PathBuf> = read_dir(p_pattern_dir_path).unwrap()
+        .map(|res| res.unwrap().path())
+        .filter(|path| !path.is_dir() && path.extension().unwrap_or(OsStr::new("")) == "json")
+        .collect();
 
-    let p_template_file_path = cli_options.value_of("template").unwrap();
-    let template_file = File::open(p_template_file_path).unwrap();
-    let template_reader = BufReader::new(template_file);
-    let template_lines: Vec<Vec<i32>> = serde_json::from_reader(template_reader).unwrap();
+    let p_template_dir_path = cli_options.value_of("template_dir").unwrap();
+    let mut template_path_list: Vec<PathBuf> = read_dir(p_template_dir_path).unwrap()
+        .map(|res| res.unwrap().path())
+        .filter(|path| !path.is_dir() && path.extension().unwrap_or(OsStr::new("")) == "json")
+        .collect();
 
-    let fit_lines = fit_template(pattern_lines, template_lines);
+    let p_output_dir_path = cli_options.value_of("output_dir").unwrap();
+    let output_dir_path = Path::new(p_output_dir_path);
 
-    if is_solvable(&fit_lines) {
-        let json = serde_json::to_string(&fit_lines);
-        println!("{:}", json.unwrap());
-    } else {
-        println!("unsolvable");
+    let mut rng = rand::thread_rng();
+
+    for pattern_path in pattern_path_list {
+        let pattern_file = File::open(&pattern_path).unwrap();
+        let pattern_reader = BufReader::new(pattern_file);
+        let pattern_lines: Vec<Vec<i32>> = serde_json::from_reader(pattern_reader).unwrap();
+        let pattern_file_name = pattern_path.file_name().unwrap().to_str().unwrap();
+
+        template_path_list.shuffle(&mut rng);
+        for template_path in &template_path_list {
+            let template_file = File::open(&template_path).unwrap();
+            let template_reader = BufReader::new(template_file);
+            let template_lines: Vec<Vec<i32>> = serde_json::from_reader(template_reader).unwrap();
+            let template_index = template_path.file_name().unwrap().to_str().unwrap().replace(".json", "");
+        
+            let fit_lines = fit_template(&pattern_lines, template_lines);
+        
+            if is_solvable(&fit_lines) {
+                let json = serde_json::to_string(&fit_lines).unwrap()
+                    .replace("[[", "[\n    [")
+                    .replace("],", "],\n    ")
+                    .replace("]]", "]\n]");
+    
+                let output_file_path = output_dir_path.join(format!("{}_{}", template_index, pattern_file_name));
+                let mut f = BufWriter::new(File::create(output_file_path).unwrap());
+                f.write_all(json.as_bytes()).unwrap();
+                f.flush().unwrap();
+                println!("resolved: {}_{}", template_index, pattern_file_name);
+                break;
+            } else {
+                println!("unsolvable: {}_{}", template_index, pattern_file_name);
+            }
+        }
     }
 }
